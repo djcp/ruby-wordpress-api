8f031fff46188daa288a425859770f2c857ef677
pseudomuto <david.muto@gmail.com>
2014-01-18 13:27:50 -0500

    loading .env if it exists for rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,9 +5,28 @@ require 'rubypress'
 require 'tempfile'
 require_relative 'vcr_setup'
 
-RSpec.configure do |config|
+def load_env(filename = '.env')
+  return unless File.exists?(filename)
 
+  File.foreach(filename) do |line|
+    next if line.chomp.size == 0
+
+    setting = line.split('=')
+    key = setting.shift
+    ENV[key] = setting.join('').chomp
+  end
 end
 
-CLIENT = Rubypress::Client.new(:host => ENV['WORDPRESS_HOST'], :port => 80, :username => ENV['WORDPRESS_USERNAME'], :password => ENV['WORDPRESS_PASSWORD'], :use_ssl => false)
+RSpec.configure do |config|
+  load_env
+end
+
+CLIENT = Rubypress::Client.new(
+  :host => ENV['WORDPRESS_HOST'],
+  :port => 80,
+  :username => ENV['WORDPRESS_USERNAME'],
+  :password => ENV['WORDPRESS_PASSWORD'],
+  :use_ssl => false
+)
+
 STRING_NUMBER_REGEX = /^[-+]?[0-9]+$/
