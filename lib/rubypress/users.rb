0d659912a1c309e5b5cbf7d60c58627e40b9cbc5
Kieran Pilkington <kieran776@gmail.com>
2014-05-13 17:25:54 +1200

    Nest modules within rubypress namespace

--- lib/rubypress/users.rb
+++ lib/rubypress/users.rb
@@ -1,40 +1,44 @@
-module Users
+module Rubypress
 
-  def getUsersBlogs
-    self.connection.call('wp.getUsersBlogs', self.username, self.password)
-  end
+  module Users
 
-  def getUser(options = {})
-    default_options = {
-      :user_id => nil,
-      :fields => []
-    }.deep_merge!(options)
-    execute('getUser', default_options)
-  end
+    def getUsersBlogs
+      self.connection.call('wp.getUsersBlogs', self.username, self.password)
+    end
 
-  def getUsers(options = {})
-    default_options = {
-      :filter => {}
-    }.deep_merge!(options)
-    execute('getUsers', default_options)
-  end
+    def getUser(options = {})
+      default_options = {
+        :user_id => nil,
+        :fields => []
+      }.deep_merge!(options)
+      execute('getUser', default_options)
+    end
 
-  def getProfile(options = {})
-    default_options = {
-      :fields => []
-    }.deep_merge!(options)
-    execute('getProfile', default_options)
-  end
+    def getUsers(options = {})
+      default_options = {
+        :filter => {}
+      }.deep_merge!(options)
+      execute('getUsers', default_options)
+    end
 
-  def editProfile(options = {})
-    default_options = {
-      :content => {}
-    }.deep_merge!(options)
-    execute('editProfile', default_options)
-  end
+    def getProfile(options = {})
+      default_options = {
+        :fields => []
+      }.deep_merge!(options)
+      execute('getProfile', default_options)
+    end
 
-  def getAuthors(options = {})
-    execute('getAuthors', options)
+    def editProfile(options = {})
+      default_options = {
+        :content => {}
+      }.deep_merge!(options)
+      execute('editProfile', default_options)
+    end
+
+    def getAuthors(options = {})
+      execute('getAuthors', options)
+    end
+
   end
 
 end
